301d078dfb85124b04461e0c153dbf61dede5678
cb653705 <cb653705@MI103-12>
2018-10-12 10:56:59 +0200

    Bien sympa.

---
+++ tp1/src/main/java/Modele/FruitSimple.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+/**
+ *
+ * @author cb653705
+ */
+public abstract class FruitSimple {
+    private double prix;
+    private String origine;
+    
+    public double getPrix()
+    {
+        return prix;
+    }
+    
+   
+}
---
+++ tp1/src/main/java/Modele/IFruit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+/**
+ *
+ * @author cb653705
+ */
+public interface IFruit {
+    public double getPrix();
+    public String getOrigine();
+    public boolean Equals(Object o);
+    public String ToString();
+}
